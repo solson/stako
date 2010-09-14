bc88b273f36bec4ea3c9df11094d52d894fc37da
Scott Olson <scott@scott-olson.org>
2010-09-13 23:12:07 -0600

    Added more stack shufflers and added stack effect comments.

--- src/runtime/stako.c
+++ src/runtime/stako.c
@@ -60,78 +60,147 @@ void StakoStack_delete(StakoStack *this) {
 }
 
 /* Stako builtins */
+// ( x -- )
 void StakoPrimitive_drop(StakoStack *stack) {
     StakoStack_pop(stack);
 }
 
+// ( x y -- )
 void StakoPrimitive_2drop(StakoStack *stack) {
     StakoStack_pop(stack);
     StakoStack_pop(stack);
 }
 
+// ( x y z -- )
 void StakoPrimitive_3drop(StakoStack *stack) {
     StakoStack_pop(stack);
     StakoStack_pop(stack);
     StakoStack_pop(stack);
 }
 
+// ( x -- x x )
 void StakoPrimitive_dup(StakoStack *stack) {
     StakoStack_push(stack, StakoStack_peek(stack));
 }
 
+// ( x y -- x y x y )
 void StakoPrimitive_2dup(StakoStack *stack) {
     StakoStack_push(stack, StakoStack_peek_index(stack, 1));
     StakoStack_push(stack, StakoStack_peek_index(stack, 1));
 }
 
+// ( x y z -- x y z x y z )
 void StakoPrimitive_3dup(StakoStack *stack) {
     StakoStack_push(stack, StakoStack_peek_index(stack, 2));
     StakoStack_push(stack, StakoStack_peek_index(stack, 2));
     StakoStack_push(stack, StakoStack_peek_index(stack, 2));
 }
 
+// ( x y -- x x y )
+void StakoPrimitive_dupd(StakoStack *stack) {
+    StakoValue y = StakoStack_pop(stack);
+    StakoPrimitive_dup(stack);
+    StakoStack_push(stack, y);
+}
+
+// ( x y -- y )
 void StakoPrimitive_nip(StakoStack *stack) {
-    StakoValue tmp = StakoStack_pop(stack);
+    StakoValue y = StakoStack_pop(stack);
     StakoStack_pop(stack);
-    StakoStack_push(stack, tmp);
+    StakoStack_push(stack, y);
 }
 
+// ( x y z -- z )
 void StakoPrimitive_2nip(StakoStack *stack) {
-    StakoValue tmp = StakoStack_pop(stack);
+    StakoValue z = StakoStack_pop(stack);
     StakoStack_pop(stack);
     StakoStack_pop(stack);
-    StakoStack_push(stack, tmp);
+    StakoStack_push(stack, z);
 }
 
+// ( x y -- x y x )
+void StakoPrimitive_over(StakoStack *stack) {
+    StakoStack_push(stack, StakoStack_peek_index(stack, 1));
+}
+
+// ( x y z -- x y z x )
+void StakoPrimitive_pick(StakoStack *stack) {
+    StakoStack_push(stack, StakoStack_peek_index(stack, 2));
+}
+
+// ( x y z -- y z x )
+void StakoPrimitive_rot(StakoStack *stack) {
+    StakoValue z = StakoStack_pop(stack);
+    StakoValue y = StakoStack_pop(stack);
+    StakoValue x = StakoStack_pop(stack);
+    StakoStack_push(stack, y);
+    StakoStack_push(stack, z);
+    StakoStack_push(stack, x);
+}
+
+// ( x y z -- z x y )
+void StakoPrimitive___MINUS__rot(StakoStack *stack) {
+    StakoValue z = StakoStack_pop(stack);
+    StakoValue y = StakoStack_pop(stack);
+    StakoValue x = StakoStack_pop(stack);
+    StakoStack_push(stack, z);
+    StakoStack_push(stack, x);
+    StakoStack_push(stack, y);
+}
+
+// ( x y -- y x )
+void StakoPrimitive_swap(StakoStack *stack) {
+    StakoValue y = StakoStack_pop(stack);
+    StakoValue x = StakoStack_pop(stack);
+    StakoStack_push(stack, y);
+    StakoStack_push(stack, x);
+}
+
+// ( x y z -- y x z )
+void StakoPrimitive_swapd(StakoStack *stack) {
+    StakoValue z = StakoStack_pop(stack);
+    StakoValue y = StakoStack_pop(stack);
+    StakoValue x = StakoStack_pop(stack);
+    StakoStack_push(stack, y);
+    StakoStack_push(stack, x);
+    StakoStack_push(stack, z);
+}
+
+// ( n -- )
 void StakoPrimitive_pp(StakoStack *stack) {
     size_t x = StakoValue_toInt(StakoStack_pop(stack));
     printf("%zi\n", x);
 }
 
+// ( x y -- x*y )
 void StakoPrimitive_fixnum__MULT__(StakoStack *stack) {
     size_t y = StakoValue_toInt(StakoStack_pop(stack));
     size_t x = StakoValue_toInt(StakoStack_pop(stack));
     StakoStack_push(stack, StakoValue_fromInt(x * y));
 }
 
+// ( x y -- x+y )
 void StakoPrimitive_fixnum__PLUS__(StakoStack *stack) {
     size_t y = StakoValue_toInt(StakoStack_pop(stack));
     size_t x = StakoValue_toInt(StakoStack_pop(stack));
     StakoStack_push(stack, StakoValue_fromInt(x + y));
 }
 
+// ( x y -- x-y )
 void StakoPrimitive_fixnum__MINUS__(StakoStack *stack) {
     size_t y = StakoValue_toInt(StakoStack_pop(stack));
     size_t x = StakoValue_toInt(StakoStack_pop(stack));
     StakoStack_push(stack, StakoValue_fromInt(x - y));
 }
 
+// ( x y -- x/y )
 void StakoPrimitive_fixnum__DIV__i(StakoStack *stack) {
     size_t y = StakoValue_toInt(StakoStack_pop(stack));
     size_t x = StakoValue_toInt(StakoStack_pop(stack));
     StakoStack_push(stack, StakoValue_fromInt(x / y));
 }
 
+// ( x y -- x%y )
 void StakoPrimitive_fixnum__MINUS__mod(StakoStack *stack) {
     size_t y = StakoValue_toInt(StakoStack_pop(stack));
     size_t x = StakoValue_toInt(StakoStack_pop(stack));
